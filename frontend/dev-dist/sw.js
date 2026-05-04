c5d77a426b73499c7a7458673f402ed2e6c4b87b
pranav <pranavnarsekar10@gmail.com>
2026-05-04 23:31:01 +0530

    some updates

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f87553f6'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.ftvbovslphk"
+    "revision": "0.kj69qe1gf5g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
